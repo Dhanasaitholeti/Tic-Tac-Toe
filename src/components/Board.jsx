c68f8fa1f27c94178f431f074bc3f81d4badf720
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-05-18 09:13:24 +0530

    added individual playig cpabilites

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import Square from "./Square";
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
 
   const handleSquareClikck = (pos) => {
+    if (squares[pos]) return;
+
     setSquares((currentSquares) => {
       return currentSquares.map((Squareval, position) => {
-        if (pos === position) return "X";
+        if (pos === position) return isXNext ? "X" : "O";
 
         return Squareval;
       });
     });
+    setIsXNext(!isXNext);
   };
 
   const renderSquare = (pos) => {
