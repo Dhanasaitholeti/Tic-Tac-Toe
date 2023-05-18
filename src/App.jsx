052d9046962cf94458633c592d5e75623216ce94
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-05-19 04:16:38 +0530

    added the players

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import "./styles.scss";
+import { useState } from "react";
 import Board from "./components/Board";
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+  const nextPlayer = isXNext ? "X" : "O";
+
+  const handleSquareClikck = (pos) => {
+    if (squares[pos]) return;
+
+    setSquares((currentSquares) => {
+      return currentSquares.map((Squareval, position) => {
+        if (pos === position) return isXNext ? "X" : "O";
+
+        return Squareval;
+      });
+    });
+    setIsXNext(!isXNext);
+  };
   return (
     <div className="app">
-      <Board />
+      <h1>Next player is {nextPlayer}</h1>
+      <Board squares={squares} handleSquareClikck={handleSquareClikck} />
     </div>
   );
 }
