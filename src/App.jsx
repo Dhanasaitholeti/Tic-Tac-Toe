409740ad119bdbb2dc82666b1efc6150ec2e9fa3
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-05-27 17:44:34 +0530

    added winner declaration logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./styles.scss";
 import { useState } from "react";
 import Board from "./components/Board";
+import { calculateWinner } from "./winner";
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
+
+  const winners = calculateWinner(squares);
   const nextPlayer = isXNext ? "X" : "O";
+  const statusMsg = winners
+    ? `winner is ${winners}`
+    : `Next player is ${nextPlayer}`;
+
+  console.log(winners);
 
   const handleSquareClikck = (pos) => {
     if (squares[pos]) return;
@@ -21,7 +29,7 @@ function App() {
   };
   return (
     <div className="app">
-      <h1>Next player is {nextPlayer}</h1>
+      <h1>{statusMsg}</h1>
       <Board squares={squares} handleSquareClikck={handleSquareClikck} />
     </div>
   );
