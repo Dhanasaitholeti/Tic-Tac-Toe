e08c564bdad0752903822016ee72f8374c41a648
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-05-28 10:23:42 +0530

    added Draw functionality and preventing users after winning a game

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import "./styles.scss";
 import { useState } from "react";
 import Board from "./components/Board";
 import { calculateWinner } from "./winner";
-
+import StatusMessage from "./components/StatusMessage";
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
 
   const winners = calculateWinner(squares);
-  const nextPlayer = isXNext ? "X" : "O";
-  const statusMsg = winners
-    ? `winner is ${winners}`
-    : `Next player is ${nextPlayer}`;
-
-  console.log(winners);
 
   const handleSquareClikck = (pos) => {
     if (squares[pos] || winners) return;
@@ -29,7 +23,7 @@ function App() {
   };
   return (
     <div className="app">
-      <h1>{statusMsg}</h1>
+      <StatusMessage winners={winners} isXNext={isXNext} squares={squares} />
       <Board squares={squares} handleSquareClikck={handleSquareClikck} />
     </div>
   );
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,42 @@
+const StatusMessage = ({ winners, isXNext, squares }) => {
+  const noMovesLeft = squares.every((val) => val !== null);
+  const nextPlayer = isXNext ? "X" : "O";
+  //   const statusMsg = winners
+  //     ? `winner is ${winners}`
+  //     : `Next player is ${nextPlayer}`;
+  const renderStatusMsg = () => {
+    if (winners) {
+      return (
+        <h2>
+          Winner is
+          <span className={winners == "x" ? "text-green" : "text-orange"}>
+            {winners}
+          </span>
+        </h2>
+      );
+    }
+
+    if (!winners && noMovesLeft) {
+      return (
+        <h2>
+          <span className="text-green">X</span> and{" "}
+          <span className="text-orange">O</span> tied
+        </h2>
+      );
+    }
+
+    if (!winners && !noMovesLeft) {
+      return (
+        <h2 className="status-message">
+          Next player is
+          <span className={isXNext ? "text-green" : "text-orange"}>
+            {nextPlayer}
+          </span>
+        </h2>
+      );
+    }
+  };
+  return <>{renderStatusMsg()}</>;
+};
+
+export default StatusMessage;
